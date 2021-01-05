5a10ca5237a7341c628ca49b47c00683aa89a23d
Owais Khalid <owaiskhalid@hotmail.co.uk>
2021-01-05 16:55:28 +0000

    still doing the maps component

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,17 @@ import { GoogleMap } from "@react-google-maps/api";
 const Map = () => {
   return (
     <div className="map">
-      <GoogleMap id="direction-example"></GoogleMap>
+      <GoogleMap
+        id="map-example"
+        mapContainerStyle={{
+          height: "600px",
+          width: "80%",
+        }}
+        zoom={13}
+        center={{ lat: 53.4808, lng: -2.2462 }}
+      ></GoogleMap>
     </div>
   );
 };
+
+export default Map;
