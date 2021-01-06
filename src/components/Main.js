745b80eaaf833fd2aba6205094fabe0360bff391
Alba Mateos <albmatcar@gmail.com>
2021-01-06 10:40:16 +0000

    display map with origin/destination

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,16 @@
-
-import React from "react";
-import { LoadScript } from "@react-google-maps/api";
+import React from 'react';
+import { LoadScript } from '@react-google-maps/api';
+import Map from './Map';
 
 function Main(props) {
   const API_KEY = process.env.REACT_APP_API_KEY;
   return (
-    <div className="mainContent">
-      <LoadScript
-        googleMapsApiKey={API_KEY}
-        libraries={["visualization"]}
-      ></LoadScript>
+    <div className='mainContent'>
+      <LoadScript googleMapsApiKey={API_KEY} libraries={['visualization']}>
+        <Map />
+      </LoadScript>
     </div>
   );
-
 }
 
 export default Main;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,65 @@
-import { GoogleMap } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  DirectionsService
+} from '@react-google-maps/api';
+import { useRef } from 'react';
 
 const Map = () => {
+  const getOrigin = useRef('');
+  const getDestination = useRef('');
+  const onClick = () => {};
+
   return (
-    <div className="map">
-      <GoogleMap
-        id="map-example"
-        mapContainerStyle={{
-          height: "600px",
-          width: "80%",
-        }}
-        zoom={13}
-        center={{ lat: 53.4808, lng: -2.2462 }}
-      ></GoogleMap>
+    <div className='map'>
+      <div className='map-settings'>
+        <hr className='mt-0 mb-3' />
+
+        <div className='row'>
+          <div className='col-md-6 col-lg-4'>
+            <div className='form-group'>
+              <label htmlFor='ORIGIN'>Origin</label>
+              <br />
+              <input
+                id='ORIGIN'
+                className='form-control'
+                type='text'
+                ref={getOrigin}
+                placeholder='current location'
+              />
+            </div>
+          </div>
+
+          <div className='col-md-6 col-lg-4'>
+            <div className='form-group'>
+              <label htmlFor='DESTINATION'>Destination</label>
+              <br />
+              <input
+                id='DESTINATION'
+                className='form-control'
+                type='text'
+                ref={getDestination}
+              />
+            </div>
+          </div>
+        </div>
+
+        <button className='btn btn-primary' type='button' onClick={onClick}>
+          Build Route
+        </button>
+      </div>
+      <div className='map-container'>
+        <GoogleMap
+          id='map-example'
+          mapContainerStyle={{
+            height: '150px',
+            width: '100%'
+          }}
+          zoom={13}
+          center={{ lat: 53.4808, lng: -2.2462 }}
+        ></GoogleMap>
+      </div>
     </div>
   );
 };
