32446a2d6a2fd5ae9211b353be0ff2cc769ff408
Owais Khalid <owaiskhalid@hotmail.co.uk>
2021-01-07 10:47:20 +0000

    completed gps and heat map layer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { LoadScript } from "@react-google-maps/api";
+import Map from "./Map";
 
 function Main(props) {
   const API_KEY = process.env.REACT_APP_API_KEY;
+
   return (
     <div className="mainContent">
-      <LoadScript
-        googleMapsApiKey={API_KEY}
-        libraries={["visualization"]}
-      ></LoadScript>
+      <LoadScript googleMapsApiKey={API_KEY} libraries={["visualization"]}>
+        <Map theme={props.theme} />
+      </LoadScript>
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,158 @@
-import { GoogleMap } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  DirectionsService,
+  HeatmapLayer,
+} from "@react-google-maps/api";
+import { useRef, useEffect, useState } from "react";
+import { modeNightStyle, modeDayStyle } from "./styles/MapNightMode";
+import Loading from "./Loading";
+import HomeIcon from "../icons/HomeIcon.png";
 
-const Map = () => {
+const Map = (props) => {
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [response, setResponse] = useState(null);
+  const [centre, setCentre] = useState({});
+  const [isLoading, setLoading] = useState(true);
+  const [hasError, setError] = useState(false);
+  const [messageError, setMessage] = useState("");
+  const getOrigin = useRef("");
+  const getDestination = useRef("");
+
+  useEffect(() => {
+    navigator.permissions.query({ name: "geolocation" }).then((result) => {
+      if (result.state === "granted") {
+        setLocation();
+      } else {
+        setError(true);
+        setMessage("Your browser needs access to your location");
+      }
+    });
+  }, []);
+
+  const setLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCentre({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+      setOrigin({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+      setLoading(false);
+    });
+  };
+
+  const onClick = () => {
+    if (getOrigin.current.value === "") {
+      setOrigin(centre);
+    } else {
+      setOrigin(getOrigin.current.value);
+    }
+    setDestination(getDestination.current.value);
+  };
+
+  const directionsCallback = (response) => {
+    if (response !== null) {
+      if (response.status === "OK") {
+        setResponse(response);
+      } else {
+        console.log(response, "response");
+      }
+    }
+  };
+
   return (
     <div className="map">
-      <GoogleMap
-        id="map-example"
-        mapContainerStyle={{
-          height: "600px",
-          width: "80%",
-        }}
-        zoom={13}
-        center={{ lat: 53.4808, lng: -2.2462 }}
-      ></GoogleMap>
+      {hasError && <p>{messageError}</p>}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="map-container">
+          <GoogleMap
+            id="direction-example"
+            mapContainerStyle={{
+              height: "150px",
+              width: "100%",
+            }}
+            zoom={13}
+            center={centre}
+            options={
+              props.theme === "light"
+                ? { styles: modeDayStyle }
+                : { styles: modeNightStyle }
+            }
+          >
+            <Marker position={centre} />
+            {destination !== "" && origin !== "" && (
+              <DirectionsService
+                options={{ destination, origin, travelMode: "WALKING" }}
+                callback={directionsCallback}
+              />
+            )}
+            {response !== null && (
+              <DirectionsRenderer options={{ directions: response }} />
+            )}
+            <HeatmapLayer
+              data={[
+                new window.google.maps.LatLng(37.782, -122.447),
+                new window.google.maps.LatLng(37.782, -122.445),
+                new window.google.maps.LatLng(37.782, -122.443),
+                new window.google.maps.LatLng(37.782, -122.441),
+                new window.google.maps.LatLng(37.782, -122.439),
+                new window.google.maps.LatLng(37.782, -122.437),
+                new window.google.maps.LatLng(37.782, -122.435),
+                new window.google.maps.LatLng(37.785, -122.447),
+                new window.google.maps.LatLng(37.785, -122.445),
+                new window.google.maps.LatLng(37.785, -122.443),
+                new window.google.maps.LatLng(37.785, -122.441),
+                new window.google.maps.LatLng(37.785, -122.439),
+                new window.google.maps.LatLng(37.785, -122.437),
+                new window.google.maps.LatLng(37.785, -122.435),
+              ]}
+            />
+          </GoogleMap>
+        </div>
+      )}
+      <div className="map-settings">
+        <hr className="mt-0 mb-3" />
+
+        <div className="row">
+          <div className="col-md-6 col-lg-4">
+            <div className="form-group">
+              <label htmlFor="ORIGIN">Origin</label>
+              <br />
+              <input
+                id="ORIGIN"
+                className="form-control"
+                type="text"
+                ref={getOrigin}
+                placeholder="current location"
+              />
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-4">
+            <div className="form-group">
+              <label htmlFor="DESTINATION">Destination</label>
+              <br />
+              <input
+                id="DESTINATION"
+                className="form-control"
+                type="text"
+                ref={getDestination}
+              />
+            </div>
+          </div>
+        </div>
+
+        <button className="btn btn-primary" type="button" onClick={onClick}>
+          Build Route
+        </button>
+      </div>
     </div>
   );
 };
