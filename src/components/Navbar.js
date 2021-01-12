b334fd660c3ead967baef29fbd62288ef5ace416
Alba Mateos <albmatcar@gmail.com>
2021-01-12 14:19:12 +0000

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,6 @@ import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import { withStyles } from '@material-ui/core/styles'
-import { useLocation } from 'react-router-dom'
 
 const NewTabs = withStyles({
     indicator: {
@@ -13,27 +12,32 @@ const NewTabs = withStyles({
 })(Tabs)
 
 const Navbar = (props) => {
-    const [value, setValue] = React.useState(2)
-    const location = useLocation()
+    const [value, setValue] = React.useState(0)
     const handleChange = (event, newValue) => {
         setValue(newValue)
     }
     return (
         <Paper square>
             <NewTabs
-                value={location.pathname}
+                value={value}
                 indicatorColor='primary'
                 textColor='primary'
                 onChange={handleChange}
                 aria-label='disabled tabs example'
                 className='nav'
             >
-                <Tab label='Main' to='/main' component={Link} />
-                <Tab label='User Profile' to='/user-profile' component={Link} />
+                <Tab label='Main' to='/main' component={Link} value={0} />
+                <Tab
+                    label='User Profile'
+                    to='/user-profile'
+                    component={Link}
+                    value={1}
+                />
                 <Tab
                     label='Travel Advice'
                     to='/travel-advice'
                     component={Link}
+                    value={2}
                 />
             </NewTabs>
         </Paper>
