7c4ffbb20025af62ff2fb4d265d658c03e83c342
adfharrsion <68917507+adfharrsion@users.noreply.github.com>
2021-01-13 16:06:04 +0000

    more work on warnings

--- src/components/RouteWarning.js
+++ src/components/RouteWarning.js
@@ -9,8 +9,25 @@ function RouteWarning(props) {
                 props.directionResponse.routes[0].overview_polyline
             )
         }
-        console.log(props.crimeData, 'CRIME DATA')
-        console.log(coords, 'COORDS')
+
+        const crimeCoords = props.crimeData
+        const crimeCoordsArr = []
+        crimeCoords.forEach((coordsObj) => {
+            let coordArr = []
+            coordArr.push(+coordsObj.lat.slice(0, -3))
+            coordArr.push(+coordsObj.lng.slice(0, -3))
+
+            crimeCoordsArr.push(coordArr)
+        })
+
+        const shortCoords = coords.map((coord) => {
+            let lat = +coord[0].toFixed(3)
+            let lng = +coord[1].toFixed(3)
+            return [lat, lng]
+        })
+
+        console.log(crimeCoordsArr, 'FORMATTED COORDS')
+        console.log(shortCoords, 'SHORT COORDS')
     }, [props.directionResponse, props.crimeData])
 
     return <div>Route Warning</div>
