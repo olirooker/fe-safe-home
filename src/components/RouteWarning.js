15cb528b7522b1d69cb3af859b8af30e73abac0b
adfharrsion <68917507+adfharrsion@users.noreply.github.com>
2021-01-14 09:39:54 +0000

    deletes warning

--- src/components/RouteWarning.js
+++ src/components/RouteWarning.js
@@ -11,26 +11,50 @@ function RouteWarning(props) {
         }
 
         const crimeCoords = props.crimeData
-        const crimeCoordsArr = []
+        const crimeCoordsArrStr = []
         crimeCoords.forEach((coordsObj) => {
             let coordArr = []
-            coordArr.push(+coordsObj.lat.slice(0, -3))
-            coordArr.push(+coordsObj.lng.slice(0, -3))
+            coordArr.push(coordsObj.lat.slice(0, -4))
+            coordArr.push(coordsObj.lng.slice(0, -4))
 
-            crimeCoordsArr.push(coordArr)
+            crimeCoordsArrStr.push(coordArr)
+        })
+
+        const crimeCoordsArr = crimeCoordsArrStr.map((element) => {
+            let numArr = []
+            let a = parseFloat(element[0])
+            let b = parseFloat(element[1])
+
+            numArr.push([a, b])
+
+            return numArr
         })
 
         const shortCoords = coords.map((coord) => {
-            let lat = +coord[0].toFixed(3)
-            let lng = +coord[1].toFixed(3)
-            return [lat, lng]
+            let coordA = parseFloat(coord[0].toFixed(2))
+            let coordB = parseFloat(coord[1].toFixed(2))
+
+            return [coordA, coordB]
         })
 
-        // console.log(crimeCoordsArr, 'FORMATTED COORDS')
-        // console.log(shortCoords, 'SHORT COORDS')
+        const crimeCoordSum = crimeCoordsArr.map((element) => {
+            return parseFloat(element[0] + element[1])
+        })
+
+        const routeCoordSum = shortCoords.map((element) => {
+            return element[0] + element[1]
+        })
+
+        const matches = crimeCoordSum.filter((element) => {
+            routeCoordSum.includes(element)
+        })
+
+        console.log(crimeCoordSum, 'CRIMES')
+        console.log(routeCoordSum, 'ROUTE')
+        console.log(matches, 'MATCHES')
     }, [props.directionResponse, props.crimeData])
 
-    return <div>Route Warning</div>
+    return <div></div>
 }
 
 export default RouteWarning
