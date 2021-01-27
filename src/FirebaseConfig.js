404da44f64a779c3499a0d8772a4b8bb0b8f7534
Oli Rooker <oliver.rooker@gmail.com>
2021-01-27 16:07:19 +0000

    change config details

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -8,13 +8,13 @@ import 'firebase/firestore'
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: 'AIzaSyAAZrNtQUk17CK6SWKg8FrvrlJ0d1UPKn4',
-    authDomain: 'fir-test-project-bec24.firebaseapp.com',
-    projectId: 'fir-test-project-bec24',
-    storageBucket: 'fir-test-project-bec24.appspot.com',
-    messagingSenderId: '231084931353',
-    appId: '1:231084931353:web:41362a60f57c2c5138ad88',
-    measurementId: 'G-8WWQBSMYY6',
+    apiKey: 'AIzaSyCUZaXzwyM_lrq-og7ZxjtvQyIfoXPla6c',
+    authDomain: 'safe-home-nc.firebaseapp.com',
+    projectId: 'safe-home-nc',
+    storageBucket: 'safe-home-nc.appspot.com',
+    messagingSenderId: '439238238842',
+    appId: '1:439238238842:web:667c62b3ef7272df3b11c3',
+    measurementId: 'G-M4RV9K8TR2',
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
